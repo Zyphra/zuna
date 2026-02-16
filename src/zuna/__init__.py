55ed6d3a556d8d7595ae96ad88126a650dbbf57d
Jonas Mago <jonas@zyphra.com>
2026-02-16 12:23:16 -0700

    merge getting started files to run zuna tutorial

--- src/zuna/__init__.py
+++ src/zuna/__init__.py
@@ -4,10 +4,9 @@ Zuna: EEG Foundation Model and Preprocessing Pipeline
 
 __version__ = "0.1.0"
 
-from .pipeline import run_zuna_pipeline, zuna_preprocessing, zuna_inference, zuna_pt_to_fif, zuna_plot
+from .pipeline import zuna_preprocessing, zuna_inference, zuna_pt_to_fif, zuna_plot
 
 __all__ = [
-    'run_zuna_pipeline',
     'zuna_preprocessing',
     'zuna_inference',
     'zuna_pt_to_fif',
